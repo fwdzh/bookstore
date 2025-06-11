9994c717325476e17585c57f8cf01b0643769410
fwdzh <wu_yi_@outlook.com>
2025-06-11 23:56:35 +0800

    User信息里添加了当前余额信息

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
 
-from .models import Book
+from .models import Book, UserProfile
+
 # Register your models here.
 admin.site.register(Book)
+admin.site.register(UserProfile)
+
+# @admin.register(UserProfile)
+# class UserProfileAdmin(admin.ModelAdmin):
+#     list_display = ('user', 'balance')
--- store/models.py
+++ store/models.py
@@ -6,9 +6,12 @@ from decimal import Decimal
 # Create your models here.
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
-    balance = models.DecimalField(max_digits=10, decimal_places=2, default = Decimal('0.00'))
+    balance = models.DecimalField(decimal_places=2, max_digits=10, default= Decimal('0.00'))
+    # avatar = models.ImageField(upload_to='avatars/', blank=True, null=True, verbose_name='头像')
+
+
     def __str__(self):
-        return f'{self.user.username} 的余额是 {self.balance}'
+        return f"{self.user.username} 的扩展信息"
 class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=50)
